34f73ab4c294e0d62afb3a9fbc7fe8ef958c9d0c
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-12 09:46:41 +0530

    styleing

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export const Content = () => {
-
-    var year =2025
-    var name = "Ram"
-    var isActive = true
-    var user = {
-      name:"Shyam",
-      age:30,
-      salary: 50000,
-    }
+  var year = 2025;
+  var name = "Ram";
+  var isActive = true;
+  var user = {
+    name: "Shyam",
+    age: 30,
+    salary: 50000,
+  };
   return (
-    <div>
-        <h1>CONTENT</h1>
-        <h1>HELLO ALL....</h1>
+    <div style={{ backgroundColor: "lightblue" }}>
+      <h1>CONTENT</h1>
+      <h1>HELLO ALL....</h1>
       <h1>NAME - {name}</h1>
       {year}
-      <h1>YEAR - {year}</h1>
-      <h2>active ?? {isActive == true ? "Active" :"Not Active"}</h2>
+      <h1 style={{color:"white"}}>YEAR - {year}</h1>
+      <h2>active ?? {isActive == true ? "Active" : "Not Active"}</h2>
       <h1>USER NAME = {user.name}</h1>
       <h2>user age = {user.age}</h2>
       <h3>user salary = {user.salary}</h3>
@@ -25,5 +24,5 @@ export const Content = () => {
       <h2>AT A TIME WE CAN RETURN ONLY 1 TAG</h2>
       <h2>whatever written in return statment will only display to browser</h2>
     </div>
-  )
-}
+  );
+};
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
 export const Footer = () => {
+
+    var footerStyle = {
+        backgroundColor: "lightgreen",
+        height: "50px",
+        width: "100%",
+        position: "fixed",
+        bottom: "0px",
+        textAlign: "center",
+        zIndex: "1"
+    }
+
   return (
-    <div>Footer</div>
+    <div style={footerStyle}>
+        <h1>FOOTER</h1>
+    </div>
   )
 }
