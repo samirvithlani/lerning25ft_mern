b59e53d933ddf93079f345b304c94d9486220cbe
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-19 08:59:39 +0530

    output

--- src/components/form/FormDemo1.jsx
+++ src/components/form/FormDemo1.jsx
@@ -1,14 +1,18 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 export const FormDemo1 = () => {
     const {register,handleSubmit} =useForm()
+    const [output, setoutput] = useState({})
+    const [isSubbmited, setisSubbmited] = useState(false)
     //register : input field : register..
     //handlesubmit : submit --> data process : -- functions...
     const submitHandler = (data)=>{
         //data  ={}
         console.log(data)
+        setoutput(data)
+        setisSubbmited(true)
     }
   return (
     <div style={{textAlign:"center"}}>
@@ -16,7 +20,7 @@ export const FormDemo1 = () => {
         <form onSubmit={handleSubmit(submitHandler)}>
             <div>
                 <label>NAME</label>
-                <input type='text' {...register("firstName")}></input>
+                <input type='text' {...register("name")}></input>
             </div>
             <div>
                 <label>AGE</label>
@@ -45,9 +49,28 @@ export const FormDemo1 = () => {
                 </select>
             </div>
             <div>
+                <label>COLOR</label>
+                <input type='color' {...register("color")}></input>
+            </div>
+            <div>
                 <input type='submit'></input>
             </div>
         </form>
+        {
+            isSubbmited && <div style={{color:output.color}}>
+            <h1>OUTPUT </h1>
+            <h1>NAME = {output.name}</h1>
+            <h2>AGE = {output.age}</h2>
+            <h3>Gender = {output.gender}</h3>
+            <h3>Country = {output.country}</h3>
+            {
+                output.hobbies.map((hobby)=>{
+                    return <li>{hobby}</li>
+                })
+            }
+        </div>
+        }
+        
     </div>
   )
 }
