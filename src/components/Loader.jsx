34680ae2a2692904d6d7464bfe77391cc5870ece
samirvithlani <samir.vithlani83955@gmail.com>
2025-05-23 09:13:49 +0530

    loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../assets/css/loader.css"
+export const Loader = () => {
+  return (
+    <div class="loader-container">
+
+    <div class="banter-loader">
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+</div>
+</div>
+
+  )
+}
--- src/components/api/ApiDemo1.jsx
+++ src/components/api/ApiDemo1.jsx
@@ -1,12 +1,17 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import { Loader } from '../Loader'
+
+
 
 export const ApiDemo1 = () => {
     //axios
     const [message, setmessage] = useState("")
     const [users, setusers] = useState([])
+    const [isLoading, setisLoading] = useState(false)
     const getUserData = async()=>{
 
+        setisLoading(true)
         const res = await axios.get("https://node5.onrender.com/user/user")
         //res --> axios object..
         console.log("axios object..",res)
@@ -15,19 +20,40 @@ export const ApiDemo1 = () => {
         console.log(res.data.data)
         setmessage(res.data.message)
         setusers(res.data.data)
+        setisLoading(false)
     }
   return (
     <div style={{textAlign:"center"}}>
         <h1>API DEMO 1</h1>
+        {
+            isLoading && <Loader/>
+        }
         <button onClick={()=>{getUserData()}}>GET</button>
         {message}
-        {
-            users.map((user)=>{
-                return <ul>
-                    <li>{user.name} - {user.age}</li>
-                </ul>
-            })
-        }
+       <table className='table table-dark'>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>AGE</th>
+                <th>EMAIL</th>
+                <th>STATUS</th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                users.map((user)=>{
+                    return<tr>
+                        <td>{user._id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.age}</td>
+                        <td>{user.email}</td>
+                        <td>{user.isActive ? "Active" :"NOt Active"}</td>
+                    </tr>
+                })
+            }
+        </tbody>
+       </table>
     </div>
   )
 }
