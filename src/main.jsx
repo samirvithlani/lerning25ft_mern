c7093aff23660d31677b69e3c62afb54a519ea74
samirvithlani <samir.vithlani83955@gmail.com>
2025-06-06 08:34:54 +0530

    persisit

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store.js";
+import { persistor, store } from "./store.js";
+import { PersistGate } from "redux-persist/integration/react";
 //import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
--- src/store.js
+++ src/store.js
@@ -2,9 +2,38 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./redux/CartSlice"
 import bankReducer from "./redux/BankSlice"
 
-export const store = configureStore({
-    reducer:{
-        cart:cartReducer,
-        bank:bankReducer
-    }
+import { combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage"
+import { persistReducer, persistStore, PURGE } from "redux-persist";
+
+
+// export const store = configureStore({
+//     reducer:{
+//         cart:cartReducer,
+//         bank:bankReducer
+//     }
+// })
+
+const rootReducer = combineReducers({
+    cart:cartReducer,
+    bank:bankReducer
+
 })
+
+const persistConfig ={
+    key:"root",
+    storage,
+    whitelist:['bank']
+}
+export const store = configureStore({
+    reducer: persistReducer(persistConfig,rootReducer),
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [PURGE],
+      },
+    }),
+
+})
+export const persistor = persistStore(store)
+
